235b434b54ac3e5c6928abb85bd0f1a6e373e1b9
liyang <1301934707.com>
2019-03-12 16:27:25 +0800

    hmx-user项目取消boot内置tomcat

--- hmx-user/src/main/java/com/hmx/MyApplication.java
+++ hmx-user/src/main/java/com/hmx/MyApplication.java
@@ -3,12 +3,30 @@ package com.hmx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//@MapperScan(value = {"com.hmx.*.dao"})
+//public class MyApplication {
+//
+//	public static void main(String[] args){
+//		SpringApplication.run(MyApplication.class, args);
+//	}
+//}
 
 @SpringBootApplication
 @MapperScan(value = {"com.hmx.*.dao"})
-public class MyApplication {
-
-	public static void main(String[] args){
+public class MyApplication extends SpringBootServletInitializer{
+	
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MyApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
+	}
 }
+
