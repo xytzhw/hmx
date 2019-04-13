a410a09b65007fd2704a859bde4f70a180c997ee
xiaoyt <1305062583@qq.com>
2019-04-13 16:24:01 +0800

    update

--- hmx-admin/src/main/java/com/hmx/common/config/SimpleLoginSuccessHandler.java
+++ hmx-admin/src/main/java/com/hmx/common/config/SimpleLoginSuccessHandler.java
@@ -1,7 +1,6 @@
 package com.hmx.common.config;
 
 import com.hmx.user.dao.HmxUserMapper;
-import com.hmx.user.dao.UserModelMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
--- hmx-core/src/main/java/com/hmx/user/service/impl/LoginServiceImpl.java
+++ hmx-core/src/main/java/com/hmx/user/service/impl/LoginServiceImpl.java
@@ -2,8 +2,15 @@ package com.hmx.user.service.impl;
 
 
 import com.hmx.data.LoginButtonData;
+import com.hmx.movie.dao.HmxMovieMapper;
+import com.hmx.movie.entity.HmxMovieExample;
+import com.hmx.movie.service.HmxMovieService;
+import com.hmx.user.dao.HmxUserMapper;
 import com.hmx.user.dao.PermissionMapper;
+import com.hmx.user.dao.UserModelMapper;
+import com.hmx.user.entity.HmxUserExample;
 import com.hmx.user.entity.po.Permission;
+import com.hmx.user.entity.po.UserModelExample;
 import com.hmx.user.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,15 +18,17 @@ import org.springframework.stereotype.Service;
 
 import java.util.*;
 
-/**
- * @author shi
- * @create 2018-07-04  21:43
- * @description
- **/
+
 @Service
 public class LoginServiceImpl implements LoginService {
     @Autowired
     private PermissionMapper permissionDao;
+
+    @Autowired
+    private HmxMovieMapper hmxMovieMapper;
+
+    @Autowired
+    private HmxUserMapper hmxUserMapper;
 
     @Override
     public List<LoginButtonData> loginButton(int userId) {
@@ -44,7 +53,16 @@ public class LoginServiceImpl implements LoginService {
     @Override
     public Map<String, Integer> loadHomeStatistics() {
         Map<String, Integer> map = new HashMap<String, Integer>();
+        //电影数统计
+        HmxMovieExample hmxMovieExample = new HmxMovieExample();
+        hmxMovieExample.createCriteria().andStateEqualTo(0);
+        int movieCount = hmxMovieMapper.countByExample(hmxMovieExample);
+        map.put("movieCount",movieCount);
+        //用户数量统计
+        HmxUserExample hmxUserExample = new HmxUserExample();
+        hmxUserExample.createCriteria().andStateEqualTo(0);
+        int userCount = hmxUserMapper.countByExample(hmxUserExample);
+        map.put("userCount",userCount);
         return map;
-
     }
 }
